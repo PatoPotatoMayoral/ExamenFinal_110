2f8f43de97d7e3f9e18eb06aab5d745ada36a69a
PatoPotatoMayoral <88838220+PatoPotatoMayoral@users.noreply.github.com>
2021-10-21 07:54:30 -0500

    Update exercise.py

--- assignments/Exa110_Listas/src/exercise.py
+++ assignments/Exa110_Listas/src/exercise.py
@@ -1,4 +1,20 @@
 def main():
+    x = int(input())
+    lista =[]
+    if x <= 0:
+        print('Error')
+    else:
+        for i in range (x):
+            z = int(input())
+            if z %2==0:
+                r =z**2
+                lista.append(r)
+            else :
+                y = z**3
+                lista.append(y)
+        print (lista)
+
+
     pass
 
 
